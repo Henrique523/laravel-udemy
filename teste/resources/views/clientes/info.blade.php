d52135654dd62a9e6a69f67399d4391064726fba
GUILHERME\Henrique <guilherme.hlg.eng@gmail.com>
2021-06-28 14:43:24 -0300

    teste

--- teste/resources/views/clientes/info.blade.php
+++ teste/resources/views/clientes/info.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.principal')
+
+@section('conteudo')
+
 <h3>Informações do cliente</h3>
 
 <p>ID: {{ $cliente['id'] }}</p>
@@ -5,3 +9,4 @@
 <br>
 
 <a href="{{ route('clientes.index') }}">Voltar</a>
+@endsection
