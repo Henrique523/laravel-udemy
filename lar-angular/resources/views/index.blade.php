80a1333ab85af1985c65988a5742fceb9ec4ccbd
guilherme <guilherme.hlg.eng@gmail.com>
2021-08-18 12:40:43 -0300

    commit

---
+++ lar-angular/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <base href="/">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="{{ asset('js/favicon.ico') }}">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <title>Laravel - Angular</title>
+
+        <link rel="stylesheet" href="{{ asset('js/styles.31d6cfe0d16ae931b73c.css') }}">
+    </head>
+    <body>
+        <app-root></app-root>
+        <script type="text/javascript" src="{{ asset('js/runtime.c14db2d8489041ca5339.js') }}" defer></script>
+        <script type="text/javascript" src="{{ asset('js/polyfills.39fd7136d95cf9307103.js') }}" defer></script>
+        <script type="text/javascript" src="{{ asset('js/main.803e08f5755c3790272b.js') }}" defer></script>
+    </body>
+</html>
