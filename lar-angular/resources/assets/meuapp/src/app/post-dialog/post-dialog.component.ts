7353a4862a8e54650e2f0b17316434468c12a823
guilherme <guilherme.hlg.eng@gmail.com>
2021-08-18 11:52:10 -0300

    continua desenvolvimento do modulo de frontend com angular

---
+++ lar-angular/resources/assets/meuapp/src/app/post-dialog/post-dialog.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-post-dialog',
+  templateUrl: './post-dialog.component.html',
+  styleUrls: ['./post-dialog.component.css']
+})
+export class PostDialogComponent implements OnInit {
+
+    public nomeArquivo: string = '';
+
+    public dados = {
+        post: new Post("","","","","",""),
+        arquivo: null
+    };
+
+    constructor(
+        public dialogRef: MatDialogRef<PostDialogComponent>
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    mudouArquivo(event) {
+        this.nomeArquivo = event.target.files[0].name;
+        this.dados.arquivo = event.target.files[0];
+    }
+
+    salvar() {
+        this.dialogRef.close(this.dados);
+    }
+
+    cancelar() {
+        this.dialogRef.close(null);
+    }
+}
