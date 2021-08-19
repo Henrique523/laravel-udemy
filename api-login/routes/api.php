ff06ae04554391181f195cfcd5b4e4100f8a6ace
guilherme <guilherme.hlg.eng@gmail.com>
2021-08-19 09:10:13 -0300

    continua modulo de autenticaçao via api do curso

--- api-login/routes/api.php
+++ api-login/routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('registro', 'AuthController@registro');
+    Route::post('login', 'AuthController@login');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', 'AuthController@logout');
+    });
+});
+
+Route::get('produtos', 'ProdutosController@index')
+    ->middleware('auth:api');
